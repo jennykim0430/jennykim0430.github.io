6cad833c5846f03d434f3be95de50ecffa8ca910
jennykim0430 <54871240+jennykim0430@users.noreply.github.com>
2022-03-09 16:47:23 -0500

    Add files via upload

---
+++ world-building/script.js
@@ -0,0 +1,46 @@
+var aText = new Array(
+"Wait!!!!",
+"I'm writing this letter to you!" ,
+"Please look close to us",
+"We are just as loving and loyal as any dogs",
+"and perhaps more grateful for the second chance!",
+  "Some may need a little more support, love,",
+  "and patience at first, but we are incredibly resilient",
+  "Love,",
+  "Little Paws<3",
+  
+  
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "🐾";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();
